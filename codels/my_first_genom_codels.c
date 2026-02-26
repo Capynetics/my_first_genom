cf11b235c519027c45cbb5fa7ffa6571cb2513ff
Capynetics <setpointcapybara@gmail.com>
2026-02-26 19:57:12 -0300

    fix

--- codels/my_first_genom_codels.c
+++ codels/my_first_genom_codels.c
@@ -602,23 +602,3 @@ get_string(const char *urdf_path, char **value,
 
   return genom_ok;
 }
-
-
-/** Codel get_motors
- *
- * Read the `motors` input port and copy its content into `out`.
- * Returns `genom_ok` when a sample was read, otherwise throws `e_input`.
- */
-genom_event
-get_motors(const my_first_genom_motors *motors,
-           or_joint_state *out,
-           const genom_context self)
-{
-  const or_joint_state *m;
-
-  if (motors->read(self) || !(m = motors->data(self)))
-    return my_first_genom_e_input(self);
-
-  *out = *m;
-  return genom_ok;
-}
