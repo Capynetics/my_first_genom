9cea24709c1b12e9418ddaade0d0f493f08a5627
Capynetics <setpointcapybara@gmail.com>
2026-03-02 17:37:38 -0300

    fix

--- codels/my_first_genom_main_codels.c
+++ codels/my_first_genom_main_codels.c
@@ -240,7 +240,9 @@ my_first_genom_main_control(const my_first_genom_ids_body_s *body, my_first_geno
   if (joint_data) {
     const size_t hardcoded_nj = 8;
 
-    joint_data->ts = state_data->ts;
+    gettimeofday(&tv, NULL);
+    joint_data->ts.sec = tv.tv_sec;
+    joint_data->ts.nsec = tv.tv_usec * 1000;
     joint_data->position._present = false;
     joint_data->velocity._present = false;
     joint_data->effort._present = true;
